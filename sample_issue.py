63861f34421475cad3a263cff287b717eac33617
Gunank Katre <115419530+Gunankkatre@users.noreply.github.com>
2025-08-28 10:33:28 +0530

    Revert "Introduce style issue for review exercise"

--- sample_issue.py
+++
@@ -1,3 +0,0 @@
-import math, os   # unused import, flake8 will catch this
-
-def greet(name): print("Hello, " + name + "!" + " This is a very very very very very very very very very very very long line")  # line >120 chars
