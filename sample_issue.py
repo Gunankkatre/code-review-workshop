179b4c847f6af9e5ab7c2606547bec24f566237f
GunankKatre <gunankkatre13@gmail.com>
2025-08-28 10:39:02 +0530

    Address review: fix lint issues

--- sample_issue.py
+++ sample_issue.py
@@ -1,3 +1,3 @@
 import math, os   # unused import, flake8 will catch this
 
-def greet(name): print("Hello, " + name + "!" + " This is a very very very very very very very very very very very long line")  # line >120 chars
+def greet(name): print(f"Hello,{name}!" )
